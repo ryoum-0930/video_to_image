d4fd84c8b663cc11b8c90f174e9117f9cb1f6e14
ryoum-0930 <dara.ryouma@gmail.com>
2019-09-07 15:42:08 +0900

    add move_file.py

--- move_file.py
+++ move_file.py
@@ -9,9 +9,8 @@ def main(this_path,target_path):
 if __name__ == "__main__":
     args = sys.argv
     # print(args)
-    # (移動先,移動元)
     if len(args) == 3:
-        main(args[2],args[1])
-    else:
+        main(args[2],args[1])   # (移動先,移動元)
+    else:   # 引数の書き方についてのエラー
         print('ERROR : Wrong way of writing arguments')
         print('NOTATION -> python move_file.py [Destination] [Want_Move_dir]')
