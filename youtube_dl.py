dd666ec0fc52797a7fdf5d3476391637d4d6dc17
ryouma <dara.ryouma@gmail.com>
2019-09-11 14:05:47 +0900

    version up

---
+++ youtube_dl.py
@@ -0,0 +1,15 @@
+import youtube_dl.YoutubeDL as ydl
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl':  "sample_music" + '.%(ext)s',
+    'postprocessors': [
+        {'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+         'preferredquality': '192'},
+        {'key': 'FFmpegMetadata'},
+    ],
+}
+
+ydl = youtube_dl.YoutubeDL(ydl_opts)
+info_dict = ydl.extract_info("https://www.youtube.com/watch?v=sr--GVIoluU", download=True)
